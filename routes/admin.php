d526c891c62676be4903cba1345cf6f2bedd5557
oswaldo <oswram19@hotmail.com>
2025-10-31 15:04:25 -0600

    se mod user controller , roleseeder, views/admin-user-index.blade.php ese se agrego codigo html por no instalar laravel collection

--- routes/admin.php
+++ routes/admin.php
@@ -9,8 +9,8 @@ use App\Http\Controllers\Admin\PostController;
 //se paso al controlador HomeController
 
 Route::get('',[HomeController::class,'index'])->name('admin.home');
-
-Route::resource('users', UserController::class)->names('admin.users');
+//le decimos que solo agregue index,edit,update
+Route::resource('users', UserController::class)->names('admin.users')->only(['index', 'edit', 'update']);
 Route::resource('categories', CategoryController::class)->names('admin.categories');
 Route::resource('tags', TagController::class)->names('admin.tags');
 Route::resource('posts', PostController::class)->names('admin.posts');
