9ca4de75cde1eb672b033e23ceb0de081dce7317
oswaldo <oswram19@hotmail.com>
2025-11-14 09:43:02 -0600

    se hicieron cambios en applout menus en español

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -57,7 +57,7 @@ const logout = () => {
                             <!-- acceso de admin -->
 
                                   <a :href="route('admin.home')" class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300">
-                                    Admin Panel
+                                     Panel Administrador
                                 </a>
                                 
                                
@@ -88,18 +88,18 @@ const logout = () => {
                                         <div class="w-60">
                                             <!-- Team Management -->
                                             <div class="block px-4 py-2 text-xs text-gray-400">
-                                                Manage Team
+                                                Gestionar grupo
                                             </div>
 
                                             <!-- Team Settings -->
                                             <DropdownLink v-if="$page.props.auth.user.current_team"
                                                 :href="route('teams.show', $page.props.auth.user.current_team)">
-                                                Team Settings
+                                                Opciones de grupo
                                             </DropdownLink>
 
                                             <DropdownLink v-if="$page.props.jetstream.canCreateTeams"
                                                 :href="route('teams.create')">
-                                                Create New Team
+                                                Crear nuevo grupo
                                             </DropdownLink>
 
                                             <!-- Team Switcher -->
@@ -107,7 +107,7 @@ const logout = () => {
                                                 <div class="border-t border-gray-200" />
 
                                                 <div class="block px-4 py-2 text-xs text-gray-400">
-                                                    Switch Teams
+                                                    Cambiar grupo
                                                 </div>
 
                                                 <template v-for="team in $page.props.auth.user.all_teams"
@@ -164,7 +164,7 @@ const logout = () => {
                                     <template #content>
                                         <!-- Account Management -->
                                         <div class="block px-4 py-2 text-xs text-gray-400">
-                                            Manage Account
+                                            Gestionar cuenta
                                         </div>
 
                                         <DropdownLink :href="route('profile.show')">
@@ -174,7 +174,7 @@ const logout = () => {
                                         <!-- link panel administrador  -->
 
                                         <a :href="route('admin.home')" class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
-                                            Admin Panel
+                                            Panel Administrador
                                         </a>
                                              
                                         <DropdownLink v-if="$page.props.jetstream.hasApiFeatures"
@@ -244,7 +244,7 @@ const logout = () => {
 
                         <div class="mt-3 space-y-1">
                             <ResponsiveNavLink :href="route('profile.show')" :active="route().current('profile.show')">
-                                Profile
+                                Perfil
                             </ResponsiveNavLink>
 
                             <ResponsiveNavLink v-if="$page.props.jetstream.hasApiFeatures"
@@ -264,7 +264,7 @@ const logout = () => {
                                 <div class="border-t border-gray-200" />
 
                                 <div class="block px-4 py-2 text-xs text-gray-400">
-                                    Manage Team
+                                    Gestionar grupo
                                 </div>
 
                                 <!-- Team Settings -->
@@ -284,7 +284,7 @@ const logout = () => {
                                     <div class="border-t border-gray-200" />
 
                                     <div class="block px-4 py-2 text-xs text-gray-400">
-                                        Switch Teams
+                                        Cambiar grupo
                                     </div>
 
                                     <template v-for="team in $page.props.auth.user.all_teams" :key="team.id">
