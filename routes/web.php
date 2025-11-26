d41482a0c63c2d31b71d40dba2b96c1c2d66a383
oswaldo <oswram19@hotmail.com>
2025-11-26 15:28:12 -0600

    se acorto la rutas  del controlador post de web.php

--- routes/web.php
+++ routes/web.php
@@ -52,15 +52,19 @@ Route::middleware([
     })->name('dashboard');
 
 
-    Route::get('datatable/users', [DatatableController::class, 'user'])->name('datatable.user'); //se agrega ruta index para ajax datatable
-    Route::get('datatable/categories', [DatatableController::class, 'category'])->name('datatable.category'); //ruta para ajax datatable de categorías
+    Route::get('datatable/users', [DatatableController::class, 'user'])->name('datatable.user');
+    Route::get('datatable/categories', [DatatableController::class, 'category'])->name('datatable.category');
+    
 
-    Route::resource('posts', PostController::class)->names('posts.index'); //se agrega names para nombrar la ruta index
-    Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show'); //se agrega ruta show
-    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create'); //se agrega ruta create
-    Route::post('/posts', [PostController::class, 'store'])->name('posts.store'); //se agrega ruta store
-    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit'); //se agrega ruta edit
-    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update'); //se agrega ruta update
-    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy'); //se agrega ruta destroy
+    //se acortaron las rutas del controlador de posts
+    Route::resource('posts', PostController::class)->names('posts');
+    
+    // Rutas anteriores individuales (ahora manejadas por Route::resource)
+    // Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+    // Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    // Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    // Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    // Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    // Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
 });
