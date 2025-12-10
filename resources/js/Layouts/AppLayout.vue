87948d367c7cee7ce818b8ce2be6dab0ce189a22
oswaldo <oswram19@hotmail.com>
2025-12-10 15:39:02 -0600

    dropdown.vue tenia errores pr eso no cargaba en npm  run dev

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -67,12 +67,12 @@ const logout = () => {
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
                             <div class="ms-3 relative">
                                 <!-- Teams Dropdown: Solo visible para administradores con permiso admin.home -->
-                                <Dropdown v-if="$page.props.jetstream.hasTeamFeatures && $page.props.can['admin.home']" align="right" width="60">
+                                <Dropdown v-if="$page.props.jetstream.hasTeamFeatures && $page.props.can['admin.home'] && $page.props.auth.user.current_team" align="right" width="60">
                                     <template #trigger>
                                         <span class="inline-flex rounded-md">
                                             <button type="button"
                                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                                {{ $page.props.auth.user.current_team.name }}
+                                                {{ $page.props.auth.user.current_team?.name || 'Sin equipo' }}
 
                                                 <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
                                                     fill="none" viewBox="0 0 24 24" stroke-width="1.5"
